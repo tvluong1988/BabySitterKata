ee580b89014c68ae371c560cd27bb3f1fd56d2fd
Thinh Luong <guitarslacker@gmail.com>
2016-03-07 09:50:48 -0600

    added BabySitter class init and deinit test

---
+++ BabySitterKata/BabySitter.swift
@@ -0,0 +1,21 @@
+//
+//  BabySitter.swift
+//  BabySitterKata
+//
+//  Created by Thinh Luong on 3/7/16.
+//  Copyright © 2016 Thinh Luong. All rights reserved.
+//
+
+import Foundation
+
+class BabySitter {
+  
+  // MARK: Lifecycle
+  init() {
+    print("init babysitter...")
+  }
+  
+  deinit {
+    print("deinit babysitter...")
+  }
+}
